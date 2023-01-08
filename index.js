1e5397bf0fa406149d2afd680af09a4501ae0b23
nts30 <alex0611f@gmail.com>
2023-01-08 19:45:45 +0300

    jsbasics

---
+++ index.js
@@ -0,0 +1,66 @@
+// let a
+// a = 5
+// console.log(a);
+
+// const b = 5 
+// console.log(b);
+// console.log(9823567823567)
+
+
+// const userName = 'Cdox'
+
+// console.log(`hello,${userName}`);
+
+// let isGreater = 4 > 1
+
+// alert( isGReater );
+
+// let a = null
+// console.log(a);
+// a = 5
+// console.log(a);
+
+// const pupil = {
+//     class: '11 A',
+//     sex:'no',
+//     age : 16,
+//     name: {
+//         firstName: 'Alex',
+//         secondName: 'Popov'
+//     }
+// }
+// console.log(pupil.class);
+
+// pupil.classs = 12
+
+// console.log(pupil)
+// pupil.interest = 'reading'
+// console.log(pupil);
+// delete pupil.sex
+// console.log(pupil)
+
+// const a = 7
+// const b = 4
+// console.log(a+b);
+
+// let e = 67
+// let i = 12
+// console.log(e-i);
+
+// const a = 5
+// const b = 'sdfsdfdasf'
+// const c = null
+// const d = undefined
+// const e = 10n
+// const f = true
+
+// console.log(typeof f);
+
+// const num = 5
+
+// console.log(typeof `${num}`);
+// console.log(typeof `String(num)`);
+// console.log(num + 'hello');
+
+const a = prompt()
+console.log(a + 1);
