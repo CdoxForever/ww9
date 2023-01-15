c3ee07e5afd54c0bf695a5d73416e860b5484a19
nts30 <alex0611f@gmail.com>
2023-01-15 19:55:56 +0300

    pain

---
+++ index.js
@@ -0,0 +1,100 @@
+// const str = 'mOre Guys Dansing On m STage'
+
+// console.log(str.toLocaleUpperCase());//смотрит мое метсоположение и че делать  с им
+
+// console.log(str.toLowerCase());
+
+// console.log(str.repeat(3));
+
+// console.log(str.includes('mOre'));//показывает тру или фолс смотря есть  ли там чтото
+// console.log(str.toLocaleLowerCase().includes('mOre'));
+
+// const str2 = 'string 2'
+
+// console.log(str.concat(' ',str2,));//concat соеденяет константы иль чето другое
+
+// console.log(str.startsWith('mOre'));
+// console.log(str.endsWith('3456'));
+
+// console.log(str.replaceAll(' ','CART'));
+
+// const a = ''
+
+// console.log(str.charAt(5));//  показывает символ или что то иное с индексом числафа ввеженого туда
+
+// console.log(str.indexOf('m'));//ищет символ первый
+// console.log(str.lastIndexOf('m'));//ищет символ последний
+
+// console.log(str.slice(3, 6));
+
+// console.log(str.substr(5, 6));
+
+// const massiv = [1,2,3,4  , 'werfwerewr' , null, {a:5} [0,1,2,3,4,5,96]]// массив это упорядочное кол значений
+// console.log(massiv);
+
+// const  number = [1,2,3,4,5,6]
+// const  number2 = [7,8,]
+
+// console.log(number.concat(number2));
+
+// // const str3 = 'qwerty'
+// // const str45 = str3.split() // split делате это элемепнтом массива
+// // console.log(str45);
+
+// const str3 = 'qwerty dfgh asdf'
+// const str45 = str3.split(' ')
+// console.log(str45);
+
+// const arr3 = [1,4,5,2,3,5]
+// const strArr3 = arr3.join('ni') 
+// console.log(strArr3);
+
+// 'const arr4 = [0,1,2,3,4,5,6,7,8,9]'
+// console.log(arr4.reverse());
+// console.log(arr4.includes(4));
+
+
+// arr4.push(10,11,12)// push добавляет в конец массива то тчо наипсано в конце
+// console.log(arr4);
+// const pop = arr4.pop()// вырезает кол-во элшеметнлов в конце массива
+// console.log(arr4);
+// console.log(pop);
+
+// arr4.unshift(3,2,1)// вырезает из начала массива
+// console.log(arr4);
+// const shift = arr4.shift()
+// console.log(arr4);
+// console.log(shift);
+
+
+// const arr5 = [23,734,78,243,4,234,754, 1000, 890, 889]
+// console.log(arr5.sort((a,b) => a - b));
+
+// console.log(arr4.slice(4,7));
+// console.log(arr4);
+// console.log('----------');
+// console.log(arr4.splice(4,6));
+// console.log(arr4);
+
+// console.log(arr4.splice(4,3, 40, 50, 60));
+// console.log(arr4);
+
+
+
+
+
+// задачи
+//1
+// const inner = prompt('write to get caps lock')
+// console.log(inner.toLocaleUpperCase());
+//2
+const myName = prompt()
+console.log(myName);
+const imyafami = myName.split(' ')
+console.log(myName);
+imyafami [0]= imyafami[0].toLocaleLowerCase()
+imyafami [1]= imyafami[1].toLocaleUpperCase()
+console.log(imyafami);
+const newmyname = imyafami.join(' ')
+console.log(newmyname);
+// 3
