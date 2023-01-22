09d625592b6749277188e7c2d63b318f61e01c48
nts30 <alex0611f@gmail.com>
2023-01-22 19:53:21 +0300

    sdf

--- index.js
+++ index.js
@@ -1,145 +1,86 @@
-// const str = 'mOre Guys Dansing On m STage'
+// const age = prompt()
 
-// console.log(str.toLocaleUpperCase());//смотрит мое метсоположение и че делать  с им
+// if (age > 18) {
+//     console.log('true');
+// } else if (age < 18) {
+//     console.log('false');
+// } else {
+//     console.log('=18');
+// }
+// ==//равны ли они несмотрч на тип даннгых
+// ===//равны лдт они смотря на ьтип данных
+// !==//смотрит несмотяр на тип даных и это знаичт не равно
+// !===//смотримт смотря на тип данных и это значит не равно
 
-// console.log(str.toLowerCase());
+// const n = 'niggers'
+// if (n != 'niggers'){
+//     console.log('yes');
+// }
 
-// console.log(str.repeat(3));
+// console.log(1 == '1');
 
-// console.log(str.includes('mOre'));//показывает тру или фолс смотря есть  ли там чтото
-// console.log(str.toLocaleLowerCase().includes('mOre'));
+// console.log(2483 % 2);
 
-// const str2 = 'string 2'
+// const userName = 'mike'
 
-// console.log(str.concat(' ',str2,));//concat соеденяет константы иль чето другое
+// switch (userName) {
+//         case 'mike':
+//         console.log('hello,mike');
+//         break;
+//         case 'nick':
+//         console.log('hello,nick');
+//         break;
+//         case 'mary':
+//         console.log('hello, mary');
+//         break;
 
-// console.log(str.startsWith('mOre'));
-// console.log(str.endsWith('3456'));
+//     default:
+//         console.log('who are you?');
+//         break;
+// }
 
-// console.log(str.replaceAll(' ','CART'));
+// const sex = 'man'
+// const age = 30
+// const haircolor = 'blue'
 
-// const a = ''
+// if (sex === 'man' || age >=30 && haircolor !== 'blue') {
+//     console.log('u r welcome');
+// } else {
+//     console.log('sorry');
+    
+// }
 
-// console.log(str.charAt(5));//  показывает символ или что то иное с индексом числафа ввеженого туда
+// let i = 0
+// while(i < 5) {
+//     alert(i)// выводит в мождалбь ном окне что в его вводят
+//     i++// прибавляет 1
+// }
 
-// console.log(str.indexOf('m'));//ищет символ первый
-// console.log(str.lastIndexOf('m'));//ищет символ последний
+// let j = 5
 
-// console.log(str.slice(3, 6));
+// do {
+//     console.log('j =',j);
+//     j++
+// }while(j< 5);                          
 
-// console.log(str.substr(5, 6));
+// for (let i = 0; i < 100; i++) {
+//     if ( i % 2 !== 0)// чьтобы было четное иль нечетное 
+//     {
+//         console.log(i);
+//     }
+// } 
 
-// const massiv = [1,2,3,4  , 'werfwerewr' , null, {a:5} [0,1,2,3,4,5,96]]// массив это упорядочное кол значений
-// console.log(massiv);
+const arr = [0,1,2,3,4,5,6,7,8,9]
+// for (let i = 0; i < arr.length; i++) {
+//     if ( arr[i] % 2 === 0 &&  i % 2 !== 0){
+//           console.log(arr[i]); 
+//     }
+// }
 
-// const  number = [1,2,3,4,5,6]
-// const  number2 = [7,8,]
+let sum = 0
 
-// console.log(number.concat(number2));
-
-// // const str3 = 'qwerty'
-// // const str45 = str3.split() // split делате это элемепнтом массива
-// // console.log(str45);
-
-// const str3 = 'qwerty dfgh asdf'
-// const str45 = str3.split(' ')
-// console.log(str45);
-
-// const arr3 = [1,4,5,2,3,5]
-// const strArr3 = arr3.join('ni') 
-// console.log(strArr3);
-
-// 'const arr4 = [0,1,2,3,4,5,6,7,8,9]'
-// console.log(arr4.reverse());
-// console.log(arr4.includes(4));
-
-
-// arr4.push(10,11,12)// push добавляет в конец массива то тчо наипсано в конце
-// console.log(arr4);
-// const pop = arr4.pop()// вырезает кол-во элшеметнлов в конце массива
-// console.log(arr4);
-// console.log(pop);
-
-// arr4.unshift(3,2,1)// вставлчет  начала массива
-// console.log(arr4);
-// const shift = arr4.shift()
-// console.log(arr4);
-// console.log(shift);
-
-
-// const arr5 = [23,734,78,243,4,234,754, 1000, 890, 889]
-// console.log(arr5.sort((a,b) => a - b));
-
-// console.log(arr4.slice(4,7));
-// console.log(arr4);
-// console.log('----------');
-// console.log(arr4.splice(4,6));
-// console.log(arr4);
-
-// console.log(arr4.splice(4,3, 40, 50, 60));
-// console.log(arr4);
-
-
-
-
-
-// задачи
-//0
-// const inner = prompt('write to get caps lock')
-// console.log(inner.toLocaleUpperCase());
-//01
-// const myName = prompt()
-// console.log(myName);
-// const imyafami = myName.split(' ')
-// console.log(myName);
-// imyafami [0]= imyafami[0].toLocaleLowerCase()
-// imyafami [1]= imyafami[1].toLocaleUpperCase()
-// console.log(imyafami);
-// const newmyname = imyafami.join(' ')
-// console.log(newmyname);
-// 1
-// const str1 ='Hello'
-// const str2='world!'
-// const str3=','
-// console.log(str1.concat(str3,' ',str2,));
-//2
-// let a = 10
-// let b = 2 
-// let c = 5
-// console.log(a+c+b);
-//3
-// const a = 17
-// const b = 10
-// const c = a-b
-// const d = 7
-// const result = c + d
-// console.log(result);
-//4
-// const str = 'I learn javascript!'
-// console.log(str.replaceAll(' ','_'));
-//5
-// let str = 'I learn javascript!'
-// console.log(str.length); 
-//6
-// const arr = [1,2,3,4]
-// const arr1 = arr[0] + arr[1]
-// const arr2 = arr[2] + arr[3]
-// const result = arr1 + arr2 
-// console.log(result);
-//7 undone
-// const str = 'Little-cutie-kittens'
-// const oddstr = (str) = str % 2 !== 0;
-// const oddstr1 = oddstr.toLocaleUpperCase()
-// console.log(oddstr1);
-//8
-// const arr = ['a']
-// arr.push( 'y', 't', 'r', 'e', 'w', 'q')
-// arr.unshift('q', 'w', 'e', 'r', 't', 'y',)
-// console.log(arr)
-//9
-// const myNameAge = prompt()
-// console.log(myNameAge);
-// const imyafami = myNameAge.split(' ')
-// const str = `Hello ${imyafami[0]}! You are ${imyafami[1]} years`
-// console.log(str);
+for (let i = 0; i < arr.length; i++){
+    sum += arr[i]
+    console.log(sum);
+}
 
