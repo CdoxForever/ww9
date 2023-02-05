96f944e60dd67c89a76d52865691191602bcaa04
nts30 <alex0611f@gmail.com>
2023-02-05 19:53:45 +0300

    imtired

--- index.js
+++ index.js
@@ -1,58 +1,117 @@
+// const obj = {
+//     firstname: 'lalalas',
+//     secondname:'sdfjsdf',
+//     info : function(phone, email) {
+//         console.log(`${this.firstname} ${this.secondname}`);//this именно этот чт4ука
+//         if(phone && email){
+//         console.log('phone', phone);
+//         console.log('Email', email);   
+//         }
 
-// //1
-// const arr = [5,2,8,25,0,5,68,2]
-// console.log("Min:", Math.min(...arr));
-// // console.log("Max:", Math.max(...arr));
+//     }    
+// }
+// obj.info()
 
-// //2
-// function diferent(str){
-//     for(i=0; i < str.length; i++){
-//         if (str.split(str[i]).length-1 > 1) 
-//         {
+// const o1 = {
+//     firstname: 'mikr',
+//     secondname: 'ertertert'
 
 // }
-// str = 'i like little kittens'
 
-//4
-// function factorial(n) {
-//     return (n != 1) ? n * factorial(n - 1) : 1;
-// }
-// console.log(factorial(13));
+//  const bindFunction =  obj.info.bind(o1)//bind соззждает новую на основе контекста, отнтсительно чего смотрим нужно вызывать
+//  bindFunction('1-111-111-1', 'emaiol@gmaoil')
 
-//5
-// const arr = [];
-// while (arr.length < 10) {
-//     const a = Math.floor(Math.random() * 100) + 1;
-//     if (arr.indexOf(a) === -1) arr.push(a);
-// }
-// console.log(arr);
-// mas()
-
-// function mas() {
-//     arr.sort((a, b) => a < b ? 1 : a > b ? -1 : 0);
-//     console.log(arr[0]);
+// const o2 = {
+//     firstname: 'peter',
+//     secondname: 'eyuirw'
 // }
 
-// function mas() {
-//     arr.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
-//     console.log(arr[0]);
+// obj.info.call(o2, '22-222-222-4', 'prerer@gmsil')// но ненужно вызывать, первым папрметром контекст дальше параметры фыункции
+
+// const o3 = {
+//     firstname: 'doctor',
+//     secondname: 'who'
+// }
+// obj.info.apply(o2,['666-666-667', 'yadnexfignyas.@opyi'])//ненужнло вывзвать, первым прасмметром котекст вторым массив аргументов для функции
+
+
+
+
+// //Promises
+// // resolve завершился учспехно reject завершился неуспешно
+// const promis = new Promise( function(resolve,reject)  {
+//     // setTimeout( () => resolve('done'), 2000 )
+//     setTimeout( () => reject('error'), 2000 )
+// })
+
+// console.log(promis);
+// promis.then(
+//     result => console.log(result)//,
+//     // error => console.log(error)
+// )
+// .catch(error => console.log(error))
+// .finally( () => console.log('finnally'))
+
+// const url = 'https://jsonplaceholder.typicode.com/users/1'
+// const f = async () => {
+//     await fetch(url)
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//       .catch (error => console.log(error))
 // }
 
-// function mas() {
-//     let sum = 0
-//     for (let i = 0; i < arr.length; i++) {
-//         sum = sum + arr[i]                      
+
+// const a = async () => {
+//     console.log('start');
+//     await f()
+//     console.log('end');
+// }
+
+// a()
+
+// const a = async () => {
+//     console.log('start');
+//      setTimeout ( () => console.log('setTimreout'), 1000 ) 
+//     console.log('end');
+// }
+
+// a()
+
+// const err = () => {
+//     throw 'some error' //создает ошибку
+// }
+
+// const tryCatch = async () => {
+//     try{
+//     await a()
+//         // err()
+//     } catch (error){
+//         console.log(error);
+//     } finally {
+//         console.log('finally');
 //     }
-//     console.log(sum);
 // }
 
-// function mas() {
-//     let sum = 0
-//     for (let i = 0; i < arr.length; i++) {
-//         sum = sum + arr[i] / 10                   
-//     }
-//     console.log(sum);
+// tryCatch()
+
+// const usersUrl = 'https://jsonplaceholder.typicode.com/users'
+// function getUsers(){
+//     fetch(usersUrl)
+//         .then(response => response.json())
+//         .then (users => console.log(users))
 // }
+// getUsers()
 
+const url = 'https://jsonplaceholder.typicode.com/users/1'
+   async function getUsers() {
+        let arr;
+         fetch(usersUrl)
+        .then(response => response.json())
+        .then(users => arr = users)
+        console.log(arr);
 
+}
+getUsers()
 
+const usersArray = getUsers()
+console.log(usersArray);
