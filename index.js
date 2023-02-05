4b11482a1274b6322b89117f04f6a19a03034f79
CdoxForever <cdoxforever@gmail.com>
2023-02-05 12:25:13 +0300

    hpmeworke

---
+++ index.js
@@ -0,0 +1,58 @@
+
+// //1
+// const arr = [5,2,8,25,0,5,68,2]
+// console.log("Min:", Math.min(...arr));
+// // console.log("Max:", Math.max(...arr));
+
+// //2
+// function diferent(str){
+//     for(i=0; i < str.length; i++){
+//         if (str.split(str[i]).length-1 > 1) 
+//         {
+
+// }
+// str = 'i like little kittens'
+
+//4
+// function factorial(n) {
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+// }
+// console.log(factorial(13));
+
+//5
+// const arr = [];
+// while (arr.length < 10) {
+//     const a = Math.floor(Math.random() * 100) + 1;
+//     if (arr.indexOf(a) === -1) arr.push(a);
+// }
+// console.log(arr);
+// mas()
+
+// function mas() {
+//     arr.sort((a, b) => a < b ? 1 : a > b ? -1 : 0);
+//     console.log(arr[0]);
+// }
+
+// function mas() {
+//     arr.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
+//     console.log(arr[0]);
+// }
+
+// function mas() {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum = sum + arr[i]                      
+//     }
+//     console.log(sum);
+// }
+
+// function mas() {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum = sum + arr[i] / 10                   
+//     }
+//     console.log(sum);
+// }
+
+
+
--- trainning/index.js
+++
@@ -1,4 +0,0 @@
-const arr = 56
-const arr3 = 76
-const arr2 = arr + arr1
-console.log(arr2);
